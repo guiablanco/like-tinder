d1dfd139a7ca7ebcbffb498ed1f32db1a11d0b62
Sebastián Aguirre <guiablanco@live.cl>
2023-07-24 18:34:32 -0400

    Cambios de formato texto page.js

--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,7 @@ import '../styles/pageText.css';
 export default function Home() {
   return (
     <main className={styles.main}>
-      <h2>Is the choosen one? </h2>
+      <h2>Is "the choosen one"?</h2>
       <StackOfCards></StackOfCards>
     </main>
   )
